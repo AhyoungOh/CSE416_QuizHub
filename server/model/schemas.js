df96af6f39b652dc2e5fef225894a39d3c91690b
Chellsea Robinson <60821695+chellseacarmel@users.noreply.github.com>
2021-10-19 13:53:11 -0400

    Schemas for backend

---
+++ server/model/schemas.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+
+const Creator = new Schema({
+    creatorId: ObjectId,
+    ownedPlatfroms: [{
+    platformName: {type: String, required: true},
+    platformDescription: String,
+    platformImage: {data: Buffer, contentType: String},
+    ownedQuizzes: [ObjectId],
+    createdDate: { type: Date, default: Date.now }
+    }],
+    creatorImage:  { type: Number, min: 0, max: 10},
+    selfIntroduction: String,
+    creatorUsername: {type: String, required: true},
+    creatorEmail: {type: String, required: true},
+    password: {type: String, required: true},
+});
+
+
+var Certificate = new Schema({
+  certificateId: ObjectId,
+  certificateRasterizedContentUrl: String,
+  certificateEncodedContent: String,
+  badgeUploadFile: {data: Buffer, contentType: String},
+  certificateRequirementsAccuracy: { type: Number, min: 0, max: 100 },
+});
+
+var Badge = new Schema({
+    badgeId: ObjectId,
+    badgeRasterizedContentUrl: String,
+    badgeEncodedContent: String,
+    badgeUploadFile: {data: Buffer, contentType: String},
+    badgeRequirementsAccuracy: { type: Number, min: 0, max: 100 },
+});
+
+
+module.exports = mongoose.model('Creator',Creator);
+module.exports = mongoose.model('Certificate',Certificate);
+module.exports = mongoose.model('Badge',Badge);
