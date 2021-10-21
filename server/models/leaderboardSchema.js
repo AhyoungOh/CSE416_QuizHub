90e1bb70fdd9665de9971b82aa268361e27115c5
AhyoungOh <ahyoung.oh@stonybrook.edu>
2021-10-21 15:36:02 -0400

    change time leaderboardschema

--- server/models/leaderboardSchema.js
+++ server/models/leaderboardSchema.js
@@ -3,15 +3,11 @@ import mongoose from 'mongoose';
 const leaderboardSchema = new mongoose.Schema({
   username: { type: String, unique: true },
   score: { type: Number },
-  time: {
-    minutes: { type: Number },
-    seconds: { type: Number },
-  },
+  minutes: { type: Number },
+  seconds: { type: Number },
   isPrivate: { type: Boolean },
-  timestamps: {
-    createdAt: { type: Date },
-    updatedAt: { type: Date },
-  },
+  createdAt: { type: Date },
+  updatedAt: { type: Date },
 });
 const Leaderboard = mongoose.model('Leaderboard', leaderboardSchema);
 
