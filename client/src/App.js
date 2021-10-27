9baef537252bda1d95ae11a81d602086eb8fcf96
YujeongShin570 <58137372+YujeongShin570@users.noreply.github.com>
2021-10-26 21:26:52 -0400

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,55 @@
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">HI</header>
-		</div>
-	);
-}
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
 
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { Provider } from 'react-redux';
+// import store from "./store";
+import Header from './components/Header';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+// import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from './components/dashboard/Dashboard';
+
+import './App.css';
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = './login';
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className='App'>
+            <Header />
+            <Route exact path='/' component={Header} />
+            <Route exact path='/signUp' component={SignUp} />
+            <Route exact path='/signIn' component={SignIn} />
+            <Switch>
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
 export default App;
