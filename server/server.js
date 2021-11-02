db3b2d41d129a448bb2c1d4229c4eadf8ec6d7fe
Kelly Peng <mingyu.peng@stonybrook.edu>
2021-11-01 22:35:42 -0400

    fixing cors error-2

--- server/server.js
+++ server/server.js
@@ -49,18 +49,6 @@ app.use(bodyParser.json());
 // back에는 user 정보를 session에 담아 쓰기 위한 설정
 // req.session.userID = userId
 
-//To prevent errors from Cross Origin Resource Sharing, we will set 
-//our headers to allow CORS with middleware like so:
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
- //and remove cacheing so we get the most recent appointments
-  res.setHeader('Cache-Control', 'no-cache');
-  next();
- });
-
 app.use(
   cookieSession({
     name: 'session',
