5ab405f683e014942c3335b1061625ce22bfd9db
Kelly Peng <mingyu.peng@stonybrook.edu>
2021-11-01 22:33:08 -0400

    fixing cors error-1

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+var bodyParser = require('body-parser');
 // import badgeRouter from './routers/badgeRouter.js';
 // import certificateRouter from './routers/certificateRouter.js';
 // import consumerRouter from './routers/consumerRouter.js';
@@ -27,18 +28,38 @@ mongoose
   .catch((err) => console.error(err));
 
 // cookie에 전달되어 오는 정보를 req.session을 통해 사용할 수 있도록 파싱해줌
-app.use(
-  // cors({
-  //   origin: 'https://cse416quizhub.herokuapp.com',
-  //   credentials: true,
-  // })
-  cors()
-);
-app.use(cookieParser());
-
+// app.use(
+//   // cors({
+//   //   origin: 'https://cse416quizhub.herokuapp.com',
+//   //   credentials: true,
+//   // })
+//   cors()
+// );
+// const corsOptions ={
+//   origin:'http://localhost:3000', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+// app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // front에는 user 정보를 cookie에 담고
 // back에는 user 정보를 session에 담아 쓰기 위한 설정
 // req.session.userID = userId
+
+//To prevent errors from Cross Origin Resource Sharing, we will set 
+//our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+ //and remove cacheing so we get the most recent appointments
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+ });
 
 app.use(
   cookieSession({
