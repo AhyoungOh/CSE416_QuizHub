2a45a8d7f087319416676adcec2dbc29fccff17e
AhyoungOh <ahyoung.oh@stonybrook.edu>
2021-11-19 01:09:40 -0500

    update playerschema name

--- src/models/playerSchema.js
+++ src/models/playerSchema.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
 
 const playerSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectID, ref: 'Consumer' },
-  username: { type: String },
+  consumerId: { type: mongoose.Schema.Types.ObjectID, ref: 'Consumer' },
+  quizId: { type: mongoose.Schema.Types.ObjectID, ref: 'Quiz' },
   score: { type: Number },
   time: {
     minutes: { type: Number },
     seconds: { type: Number },
   },
-  quizId: { type: mongoose.Schema.Types.ObjectID, ref: 'Quiz' },
 });
 const Player = mongoose.model('Player', playerSchema);
 
