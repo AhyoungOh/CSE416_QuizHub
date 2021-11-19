9d2a298900cbdb8c938d044d32e48680764b5ec0
Kelly Peng <mingyu.peng@stonybrook.edu>
2021-11-19 16:27:33 -0500

    update ui for question preview dropdown card

--- client/src/components/Question/index.js
+++ client/src/components/Question/index.js
@@ -1,4 +1,14 @@
-import './style.scss';
+// TODO: add another type of display for question edit
+import { Grid, Button, Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  buttonsContainer: {
+    paddingTop: '20px',
+    paddingBottom: '20px',
+  }
+});
 
 function Question({
   questionNumber,
@@ -7,12 +17,30 @@ function Question({
   questionOptions,
   setquestionData,
 }) {
+  const classes = useStyles();
   return (
-    <div className='question' onClick={setquestionData}>
-      <div className='questionnumber'>Question Number: {questionNumber}</div>
-      <div className='questionOptions'>Question Option: {questionOptions}</div>
-      <div className='questionAnswer'>Question Answer: {questionAnswer}</div>
-      <div className='question'>Question: {questionQuestion}</div>
+    <div className='question'>
+      <Grid container>
+        <Grid item sx={{ minWidth: '800px' }}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreRoundedIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography onClick={setquestionData}>{questionNumber}: {questionQuestion}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {questionOptions}
+              </Typography>
+              <Typography>
+                Question Answer: {questionAnswer}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Grid>
+      </Grid>
     </div>
   );
 }
