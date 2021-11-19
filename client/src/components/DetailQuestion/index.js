ab93301735be2f88d3ce8e5598e73592ca63d5d8
Kelly Peng <mingyu.peng@stonybrook.edu>
2021-11-19 16:15:02 -0500

    update ui for quiz details and question details

--- client/src/components/DetailQuestion/index.js
+++ client/src/components/DetailQuestion/index.js
@@ -1,9 +1,10 @@
 import Question from '../Question';
 import { useHistory } from 'react-router-dom';
+import { Grid, Button } from '@mui/material';
 
 function DetailQuestion({ questiondata, setQuestionVisible }) {
   const history = useHistory();
-  const updateQuetsionData = () => {
+  const updateQuestionData = () => {
     setQuestionVisible(true);
   };
   return (
@@ -13,13 +14,18 @@ function DetailQuestion({ questiondata, setQuestionVisible }) {
       >
         Back to Question List
       </button> */}
-      <Question
-        questionNumber={questiondata.questionNumber}
-        questionQuestion={questiondata.questionQuestion}
-        questionOptions={questiondata.questionOptions}
-        questionAnswer={questiondata.questionAnswer}
-      />
-      <button onClick={updateQuetsionData}>Edit Question</button>
+      {/* TODO: question edit card */}
+      <Grid sx={{ padding: '20px' }}>
+        <Question
+          questionNumber={questiondata.questionNumber}
+          questionQuestion={questiondata.questionQuestion}
+          questionOptions={questiondata.questionOptions}
+          questionAnswer={questiondata.questionAnswer}
+        />
+      </Grid>
+      <Grid sx={{ paddingLeft: '20px'}}>
+        <Button variant="contained" onClick={updateQuestionData}>Edit Question</Button>
+      </Grid>
     </div>
   );
 }
--- client/src/components/DetailQuiz/index.js
+++ client/src/components/DetailQuiz/index.js
@@ -1,24 +1,36 @@
 import Quiz from '../Quiz';
 import Question from '../Question';
 import { Link, useHistory } from 'react-router-dom';
+import { Grid, Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  buttonsContainer: {
+    paddingTop: '20px',
+    paddingBottom: '20px',
+  }
+});
 
 function DetailQuiz({ quizData, setQuizVisible }) {
   const owned = [];
   const history = useHistory();
+  const classes = useStyles();
   for (let i = 0; i < Object.keys(quizData.quizQuestions).length; i++) {
     owned.push(quizData.quizQuestions[i].questionQuestion + ', ');
   }
   const QuestionComponents = quizData.quizQuestions.map((questionData) => {
     return (
-      <Question
-        questionNumber={questionData.questionNumber}
-        questionQuestion={questionData.questionQuestion}
-        questionAnswer={questionData.questionAnswer}
-        questionOptions={questionData.questionOptions}
-        setquestionData={() => {
-          history.push(`/question/detail/${questionData._id}`);
-        }}
-      />
+      <Grid item>
+        <Question
+          questionNumber={questionData.questionNumber}
+          questionQuestion={questionData.questionQuestion}
+          questionAnswer={questionData.questionAnswer}
+          questionOptions={questionData.questionOptions}
+          setquestionData={() => {
+            history.push(`/question/detail/${questionData._id}`);
+          }}
+        />
+      </Grid>
     );
   });
   const questionNumber = [];
@@ -34,26 +46,41 @@ function DetailQuiz({ quizData, setQuizVisible }) {
   console.log('quiz questions', quizData);
   return (
     <div>
-      <button
-        onClick={() => {
-          history.push(`/creatorHome/${quizData.platformId}`);
-        }}
-      >
-        Back to Quiz List
-      </button>
+      <Grid containter sx={{ paddingLeft: "10px", paddingTop: "10px" }}>
+        <Button
+          onClick={() => {
+            history.push(`/creatorHome/${quizData.platformId}`);
+          }}
+          // startIcon={<KeyboardBackspaceRoundedIcon />}
+          sx={{ color: "gray" }}
+        >
+          Back to platform page
+        </Button>
+      </Grid>
+      {/* TODO: change it to creator preview quiz card */}
       <Quiz
         quizName={quizData.quizName}
         quizDescription={quizData.quizDescription}
         quizImage={quizData.quizImage}
         quizQuestions={quizData.quizQuestions}
       />
-      <div className='detail-content'>
-        <div className='quizlist'>{QuestionComponents}</div>
-        <div className='buttons'>
-          <button onClick={updateQuizData}>Edit Quiz</button>
-          <button onClick={updateQuestionData}>Add Quesiton</button>
-        </div>
-      </div>
+      <Grid 
+        container 
+        direction="column" 
+        spacing={2} 
+        justifyContent="center" 
+        sx={{ maxWidth: '1000px', paddingLeft: '20px' }}
+      >
+        {QuestionComponents}
+      </Grid>
+      <Grid container spacing={1} justifyContent="center" className={classes.buttonsContainer}>
+        <Grid item>
+          <Button variant="contained" onClick={updateQuizData}>Edit Quiz</Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" onClick={updateQuestionData}>Add Quesiton</Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
