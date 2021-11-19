4a62fcee8682636ace82786b2b7b18ca86edb2c9
YujeongShin570 <58137372+YujeongShin570@users.noreply.github.com>
2021-11-19 07:12:19 -0500

    deploy

--- server/server.js
+++ server/server.js
@@ -38,6 +38,7 @@ app.use(
   cors({
     origin: [
       'https://cse416-quizhub.netlify.app',
+      'https://cse416-quizhub.heroku.app',
       'http://localhost:3000',
       'http://localhost:4000',
       // 'http://localhost:5000',
