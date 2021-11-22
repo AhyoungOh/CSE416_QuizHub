440905ebf32a017120dd9f316c5cc9741d6a0a92
AhyoungOh <ahyoung.oh@stonybrook.edu>
2021-11-22 03:40:20 -0500

    correct typo

--- server/src/models/questionSchema.js
+++ server/src/models/questionSchema.js
@@ -4,6 +4,10 @@ const questionSchema = new mongoose.Schema({
   questionNumber: { type: Number },
   questionQuestion: { type: String },
   questionOptions: [{ type: String }],
+  questionOption1: { type: String },
+  questionOption2: { type: String },
+  questionOption3: { type: String },
+  questionOption4: { type: String },
   questionAnswer: { type: Number },
   quizId: { type: String },
 });
