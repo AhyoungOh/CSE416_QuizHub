c52c48cec2cb6004cb6edd5e61cf97a07f34207c
YujeongShin570 <58137372+YujeongShin570@users.noreply.github.com>
2021-12-08 02:00:19 -0500

    alert during signing up as consumer

--- client/src/components/Form/ConsumerSignUp/index.js
+++ client/src/components/Form/ConsumerSignUp/index.js
@@ -86,6 +86,15 @@ function ConsumerSignUp() {
       alert('please put the valid email');
       return;
     }
+
+    if (
+      !usernameRef.current.value ||
+      !passwordRef.current.value ||
+      !emailRef.current.value
+    ) {
+      alert('please fill out all sections');
+      return;
+    }
     try {
       const consumerInfo = await axios.post(
         process.env.NODE_ENV == 'production'
